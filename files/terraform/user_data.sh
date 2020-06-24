73353ab42181e471302ecff1e2f1522f5a28e7e6
Darren Khan <djkhan85@gmail.com>
2020-06-24 12:51:29 -0400

    updated git ignore

--- files/terraform/user_data.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-sudo yum update -y
-sudo yum install python37 python3-pip -y
-pip3 install docker
-sudo amazon-linux-extras install docker -y
-sudo service docker start
-sudo chkconfig docker on
-sudo usermod -a -G docker ec2-user
