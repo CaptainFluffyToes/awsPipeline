fcfd553e5b62d97f05553615a44e1a6dd152af25
Darren Khan <djkhan85@gmail.com>
2020-06-30 22:56:29 -0400

    update variables

--- terraform/aws/terraform.tfvars
+++ terraform/aws/terraform.tfvars
@@ -1,5 +1,7 @@
 region = "us-east-1"
 
+aws_pub_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCLF5RUExiE8ZrTTKZN2bDYfE+ReRI1WIQmwTJFCfAH5tdngQEAKIG/M+xm2LJ4gNxh/0pQfZSimsULC+eJs8xnrkWZs1CnpoHiShub2GCJeaiNJAmjFrs3OFGcv0sGW+DK+WCoVLyendfDSWjGZ4psYFPwWOy8hRY9texkR6EokgttBZVmugZcPVLHnGv86lUFoaOj+PEHzxQpKU7CezkzGzlBUUA4S4J15Oe1k0YTLdZ+dUKAxHlyOmg+xse0Qy7/edCOz7GlwiS1gyrTab225LbVxB+ePUC1CjXG4DqEuP86fPsYJEZPZLze1o0uHLtX08OvO96VNorutKOj9DlX"
+
 vpc_cidr = "172.86.21.0/26"
 
 subnet_external_cidr = "172.86.21.0/27"
@@ -13,3 +15,19 @@ name = "cyberark_hydration"
 role = "hydration"
 
 company = "cyberark"
+
+ssh_key_name = "dk-master"
+
+conjur_master_instance_type = "t2.medium"
+
+conjur_standby_instance_type = "t2.medium"
+
+conjur_follower_instance_type = "t2.medium"
+
+standby_instance_count = 3
+
+follower_instance_count = 1
+
+ansible_ami = "ami-037c4c158e6e397eb"
+
+ansible_instance_type = "t2.medium"
--- terraform/aws/variables.tf
+++ terraform/aws/variables.tf
@@ -3,6 +3,11 @@ variable "region" {
   default = "us-east-1"
 }
 
+variable "aws_pub_key" {
+  type    = string
+  default = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCLF5RUExiE8ZrTTKZN2bDYfE+ReRI1WIQmwTJFCfAH5tdngQEAKIG/M+xm2LJ4gNxh/0pQfZSimsULC+eJs8xnrkWZs1CnpoHiShub2GCJeaiNJAmjFrs3OFGcv0sGW+DK+WCoVLyendfDSWjGZ4psYFPwWOy8hRY9texkR6EokgttBZVmugZcPVLHnGv86lUFoaOj+PEHzxQpKU7CezkzGzlBUUA4S4J15Oe1k0YTLdZ+dUKAxHlyOmg+xse0Qy7/edCOz7GlwiS1gyrTab225LbVxB+ePUC1CjXG4DqEuP86fPsYJEZPZLze1o0uHLtX08OvO96VNorutKOj9DlX"
+}
+
 variable "vpc_cidr" {
   type    = string
   default = "172.86.21.0/26"
@@ -36,4 +41,44 @@ variable "role" {
 variable "company" {
   type    = string
   default = "cyberark"
+}
+
+variable "ssh_key_name" {
+  type    = string
+  default = "dk-master"
+}
+
+variable "conjur_master_instance_type" {
+  type    = string
+  default = "t2.medium"
+}
+
+variable "conjur_standby_instance_type" {
+  type    = string
+  default = "t2.medium"
+}
+
+variable "conjur_follower_instance_type" {
+  type    = string
+  default = "t2.medium"
+}
+
+variable "standby_instance_count" {
+  type    = number
+  default = 2
+}
+
+variable "follower_instance_count" {
+  type    = number
+  default = 1
+}
+
+variable "ansible_ami" {
+  type    = string
+  default = "ami-037c4c158e6e397eb"
+}
+
+variable "ansible_instance_type" {
+  type    = string
+  default = "t2.medium"
 }
