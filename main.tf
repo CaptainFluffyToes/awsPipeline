1189dab8952b953314cf779bcf4334615fd54e45
CaptainFluffyToes <djkhan85@gmail.com>
2020-07-27 13:20:29 -0400

    updating with terraform cloud and github actions.

--- main.tf
+++ main.tf
@@ -1,3 +1,15 @@
+terraform {
+  backend "remote" {
+    # The name of your Terraform Cloud organization.
+    organization = "captainfluffytoes"
+
+    # The name of the Terraform Cloud workspace to store Terraform state files in.
+    workspaces {
+      name = "awsPipeline"
+    }
+  }
+}
+
 provider "aws" {
   region = var.region
 }
